47c38361b9cb90809aa431597bdc48101a4279c8
Sebastian R <77353871+Sepl9100@users.noreply.github.com>
2024-05-25 11:35:45 +0200

    just checking if git is working properly...

--- ocr/main.py
+++ ocr/main.py
@@ -10,9 +10,14 @@ WebApp.secret_key = "supersmshackerburgsystemtoll"
 
 
 def image_processing(img_path):
-    img = cv2.imread('img_path')
+    img = cv2.imread(img_path)
+    img = cv2.medianBlur(img,5)
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     text = str(pytesseract.image_to_string(img, lang="deu", config='--oem 1 --psm 6'))
+    print("Got Text from Image. Processing...")
     return text
+
 
 @WebApp.route("/upload", methods=["POST"])
 def upload():
@@ -26,23 +31,18 @@ def upload():
 
     if file:
         filename = secure_filename(file.filename)
-        file.save(f"uploads/{filename}")
+        print(f"Received File: {filename}")
+        file.save(filename)
 
-        image_text = image_processing(f"/uploads/{filename}")
+        image_text = image_processing(filename)
         json = get_products(image_text)
-
-        return jsonify({"message": json}), 200
+        print("done")
+        return json, 200
+    print("File not uploaded")
     return jsonify({"error": "File not uploaded"}), 400
 
 
-
-
 if __name__ == '__main__':
-    WebApp.run(debug=True, port=80, host='192.168.137.1')
+    WebApp.run(port=80, host='192.168.137.1')
 
-'''
-img = cv2.imread('bill1.jpg')
-text = str(pytesseract.image_to_string(img, lang="deu", config='--oem 1 --psm 6'))
-print(text)
-get_products(text)
-'''
+
